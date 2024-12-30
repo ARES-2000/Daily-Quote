2251852bca4743aa8110169c1036bdbf3d823239
ARES-2000 <166283477+ARES-2000@users.noreply.github.com>
2024-12-30 13:16:41 +0200

    Added Web App files.

---
+++ index.js
@@ -0,0 +1,30 @@
+class quoteData{
+   quote;
+   author;
+}
+
+fetch("https://api.api-ninjas.com/v1/quotes?category=success",{
+   headers :new Headers({
+      "x-api-key" : "vRsHcNdviiqQppBMFG2Thw==aFMyzTMObfnsjjOn"
+   })
+})
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+      quoteData.quote = data[0].quote
+      quoteData.author = data[0].author
+  });
+  setTimeout(()=>{
+   console.log(quoteData.quote)
+   console.log(`'${quoteData.author}'`)
+   document.querySelector('.css-quote').innerText = `'${quoteData.quote}'`
+   document.querySelector('.css-quote-author').innerText = `-${quoteData.author}-`
+  },2000)
+
+  
+
+
+
+
+
